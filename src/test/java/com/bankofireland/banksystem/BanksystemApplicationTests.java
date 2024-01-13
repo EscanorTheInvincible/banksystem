7af359763e399abe976fa1004c54d759ba5017b4
EscanorTheInvincible <abhilashpatade09@gmail.com>
2024-01-13 19:48:11 +0000

    Initial Push

---
+++ src/test/java/com/bankofireland/banksystem/BanksystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bankofireland.banksystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BanksystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
